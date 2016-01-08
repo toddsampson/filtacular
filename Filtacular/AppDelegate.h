592ab2adb4208d1be315d8ad40f660305410ee60
Todd Sampson <tsampson@cloudspace.com>
2016-01-07 22:29:23 -0500

    Initial Commit

---
+++ Filtacular/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Filtacular
+//
+//  Created by Todd Sampson on 1/7/16.
+//  Copyright © 2016 Todd Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Filtacular/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Filtacular
+//
+//  Created by Todd Sampson on 1/7/16.
+//  Copyright © 2016 Todd Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
