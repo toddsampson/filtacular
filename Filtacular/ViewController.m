5d7732ef4efd4bed5b05594bf2c616a83eaad03f
ZaidPathan <zaidkhanintel@gmail.com>
2016-01-21 00:13:03 +0530

    Pull to refresh added

--- Filtacular/ViewController.m
+++ Filtacular/ViewController.m
@@ -19,11 +19,29 @@
     
     NSURL *url = [NSURL URLWithString:@"http://filtacular.com"];
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+    [self addPullToRefreshToWebView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)addPullToRefreshToWebView{
+    UIColor *whiteColor = [UIColor whiteColor];
+    UIRefreshControl *refreshController = [UIRefreshControl new];
+    NSString *string = @"Pull down to refresh...";
+    NSDictionary *attributes = @{ NSForegroundColorAttributeName : whiteColor };
+    NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:string attributes:attributes];
+    refreshController.bounds = CGRectMake(0, 0, refreshController.bounds.size.width, refreshController.bounds.size.height);
+    refreshController.attributedTitle = attributedString;
+    [refreshController addTarget:self action:@selector(refreshWebView:) forControlEvents:UIControlEventValueChanged];
+    [refreshController setTintColor:whiteColor];
+    [self.webView.scrollView addSubview:refreshController];
+}
+
+- (void)refreshWebView:(UIRefreshControl*)refreshController{
+    [self.webView reload];
+    [refreshController endRefreshing];
 }
 
 @end
